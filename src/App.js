8ed3bd615ac10b15a0c470b21a3a9e3da79675b5
StelOkwu <stelco83@gmail.com>
2023-07-29 03:43:14 +0200

    new date

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My React App with IBM</h1>
+      <hr></hr>
+      <h3>29th July 2023</h3>
+      <hr></hr>
     </div>
   );
 }
